5adb514788685a818b27df248ea3a787bff21f4a
maulana khasan <maulanakhasan92@gmail.com>
2025-08-13 20:18:44 +0700

    feat: add admin dashboard and sidebar components
    - Updated package.json to include new Radix UI dependencies for avatar, dialog, separator, and tooltip.
    - Created AdminDashboardPage component for admin functionalities.
    - Implemented DashboardLayout component with sidebar and dark mode toggle.
    - Developed AppSidebar component for navigation with user avatar and dropdown menu.
    - Added Avatar, Breadcrumb, Separator, Sheet, Sidebar, Skeleton, Tooltip components for UI consistency.
    - Introduced useIsMobile hook for responsive design handling.

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -13,8 +13,8 @@ export default function AuthLayout({ children }: AuthLayoutProps) {
 			</div>
 			<div className="flex w-full max-w-sm flex-col gap-6">
 				<div className="flex items-center gap-2 self-center font-medium">
-					<div className="flex items-center p-2 bg-teal-500 justify-center rounded-md">
-						<Coffee className="h-6 w-6" />
+					<div className="bg-teal-500 flex p-2 items-center justify-center rounded-md">
+						<Coffee className="size-4" />
 					</div>
 					XXX Cafe
 				</div>
---
+++ src/app/(dashboard)/admin/page.tsx
@@ -0,0 +1,11 @@
+export default function AdminDashboardPage() {
+	return (
+		<div>
+			<h1>Admin Dashboard</h1>
+			<p>
+				Welcome to the admin dashboard. Here you can manage your application.
+			</p>
+			{/* Additional admin functionalities can be added here */}
+		</div>
+	);
+}
